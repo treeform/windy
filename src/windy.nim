595dc4357b7af0986ad98ef27760421ea02b2149
Ryan Oldenburg <ryan@guzba.com>
2021-10-18 00:04:49 -0500

    macos2

--- src/windy.nim
+++ src/windy.nim
@@ -5,7 +5,7 @@ export common, vmath
 when defined(windows):
   import windy/platforms/win32/platform
 elif defined(macosx):
-  import windy/platforms/macos/platform
+  import windy/platforms/macos2/platform
 elif defined(linux):
   import windy/platforms/x11/platform
 
