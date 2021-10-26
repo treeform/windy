3dcab2556a6ef6d03902477a0ccb952380bf4a81
levovix0 <xlevovix@gmail.com>
2021-10-26 10:01:15 +0300

    remove async

--- src/windy/platforms/linux/wayland/basic.nim
+++ src/windy/platforms/linux/wayland/basic.nim
@@ -1,4 +1,4 @@
-import os, posix, nativesockets, asyncnet, asyncdispatch, tables
+import os, posix, nativesockets, net, tables
 import ../../../common
 
 type
@@ -9,10 +9,10 @@ type
     id: Id
 
   Display* = ref object of Proxy
-    onError*: proc(objId: Id, code: DisplayErrorCode, message: string): Future[void]
-    onDeleteId*: proc(id: Id): Future[void]
+    onError*: proc(objId: Id, code: DisplayErrorCode, message: string)
+    onDeleteId*: proc(id: Id)
 
-    socket: AsyncSocket
+    socket: Socket
     ids: Table[uint32, Proxy]
     lastId: Id
   
@@ -55,7 +55,7 @@ proc connect*(name = getEnv("WAYLAND_SOCKET")): Display =
   result.ids[1] = result
 
   let d = result
-  result.onDeleteId = proc(id: Id) {.async.} =
+  result.onDeleteId = proc(id: Id) =
     d.ids.del id.uint32
   
   var name =
@@ -76,8 +76,7 @@ proc connect*(name = getEnv("WAYLAND_SOCKET")): Display =
     close sock
     raise WindyError.newException("Failed to connect to wayland server")
   
-  register sock.AsyncFD
-  result.socket = newAsyncSocket(sock.AsyncFD, nativesockets.AF_UNIX, nativesockets.SOCK_STREAM, nativesockets.IPPROTO_IP)
+  result.socket = newSocket(sock, nativesockets.AF_UNIX, nativesockets.SOCK_STREAM, nativesockets.IPPROTO_IP)
 
 
 proc new(d: Display, t: type): t =
@@ -146,23 +145,23 @@ proc deserialize(x: seq[uint32], T: type): T =
   deserialize(x, T, i)
 
 
-proc marshal[T](x: Proxy, op: int, data: T) {.async.} =
+proc marshal[T](x: Proxy, op: int, data: T) =
   var d = data.serialize
   d.insert ((d.len.uint32 * uint32.sizeof.uint32 + 8) shl 16) or (op.uint32 and 0x0000ffff)
   d.insert x.id.uint32
-  await x.display.socket.send(d[0].addr, d.len * uint32.sizeof)
+  assert x.display.socket.send(d[0].addr, d.len * uint32.sizeof) == d.len * uint32.sizeof
 
-method unmarshal(x: Proxy, op: int, data: seq[uint32]) {.base, async.} = discard
+method unmarshal(x: Proxy, op: int, data: seq[uint32]) {.base, locks: "unknown".} = discard
 
 
-proc listen*(d: Display) {.async.} =
-  ## runs endless listen loop
-  while true:
-    let head = d.socket.recv(2 * uint32.sizeof).await.asSeq(uint32)
-    let id = head[0]
-    let op = head[1] and 0xffff
-    let len = int (head[1] shr 16)
-    assert len >= 8
-    let data = d.socket.recv(len - 8).await.asSeq(uint32)
-    if not d.ids.hasKey id: continue # event for destroyed object
-    await d.ids[id].unmarshal(op.int, data)
+proc pollNextEvent*(d: Display) =
+  let head = d.socket.recv(2 * uint32.sizeof).asSeq(uint32)
+  let id = head[0]
+  let op = head[1] and 0xffff
+  let len = int (head[1] shr 16)
+  assert len >= 8
+
+  let data = d.socket.recv(len - 8).asSeq(uint32)
+
+  if not d.ids.hasKey id: return # event for destroyed object
+  d.ids[id].unmarshal(op.int, data)
--- src/windy/platforms/linux/wayland/protocol.nim
+++ src/windy/platforms/linux/wayland/protocol.nim
@@ -2,55 +2,58 @@ include basic
 
 type
   Registry* = ref object of Proxy
-    onGlobal*: proc(name: Id, iface: string, version: uint32): Future[void]
-    onGlobalRemove*: proc(name: Id): Future[void]
+    onGlobal*: proc(name: Id, iface: string, version: uint32)
+    onGlobalRemove*: proc(name: Id)
 
   Callback* = ref object of Proxy
-    onDone*: proc(cbData: uint32): Future[void]
+    onDone*: proc(cbData: uint32)
 
 
 template d: Display = x.display
 
 
-proc sync*(x: Display): Future[Callback] {.async.} =
+proc sync*(x: Display): Callback =
   result = d.new(Callback)
-  await x.marshal(0, result.id)
+  x.marshal(0, result.id)
 
-proc registry*(x: Display): Future[Registry] {.async.} =
+proc registry*(x: Display): Registry =
   result = d.new(Registry)
-  await x.marshal(1, result.id)
+  x.marshal(1, result.id)
 
-method unmarshal(x: Display, op: int, data: seq[uint32]) {.async.} =
+method unmarshal(x: Display, op: int, data: seq[uint32]) =
   case op
   of 0:
     let (objId, code, message) = data.deserialize((Id, DisplayErrorCode, string))
-    if x.onError != nil: await x.onError(objId, code, message)
+    if x.onError != nil: x.onError(objId, code, message)
   of 1:
     let id = data.deserialize(Id)
-    if x.onDeleteId != nil: await x.onDeleteId(id)
+    if x.onDeleteId != nil: x.onDeleteId(id)
   else: discard
 
 
-proc bindInterface*[T: Proxy](x: Registry, name: Id, iface: string, version: uint32): Future[T] {.async.} =
+proc bindInterface*[T: Proxy](x: Registry, name: Id, iface: string, version: uint32): T =
   result = d.extern(T, name)
-  await d.marshal(0, (name, iface, version))
+  d.marshal(0, (name, iface, version))
 
-method unmarshal(x: Registry, op: int, data: seq[uint32]) {.async.} =
+method unmarshal(x: Registry, op: int, data: seq[uint32]) =
   case op
   of 0:
     let (name, iface, version) = data.deserialize((Id, string, uint32))
-    if x.onGlobal != nil: await x.onGlobal(name, iface, version)
+    if x.onGlobal != nil: x.onGlobal(name, iface, version)
   of 1:
     let name = data.deserialize(Id)
-    if x.onGlobalRemove != nil: await x.onGlobalRemove(name)
+    if x.onGlobalRemove != nil: x.onGlobalRemove(name)
   else: discard
 
 
 when isMainModule:
   let display = connect()
-  display.onError = proc(objId: Id, code: DisplayErrorCode, message: string) {.async.} =
+  display.onError = proc(objId: Id, code: DisplayErrorCode, message: string) =
     echo "Error for ", objId.uint32, ": ", code, ", ", message
-  let reg = display.registry.waitFor
-  reg.onGlobal = proc(name: Id, iface: string, version: uint32) {.async.} =
+  
+  let reg = display.registry
+  reg.onGlobal = proc(name: Id, iface: string, version: uint32) =
     echo (id: name.uint32, iface: iface, version: version)
-  waitFor listen display
+  
+  while true:
+    pollNextEvent display
