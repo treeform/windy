3a04a8df4a0003da9fcaf5f2ad2789a3ebce5a89
Andre von Houck <starplant@gmail.com>
2025-08-25 13:44:59 -0700

    Add WIP support for Emscripten. (#122)

--- src/windy.nim
+++ src/windy.nim
@@ -1,6 +1,8 @@
 import unicode, vmath, windy/common
 
-when defined(windows):
+when defined(emscripten):
+  import windy/platforms/emscripten/platform
+elif defined(windows):
   import windy/platforms/win32/platform
 elif defined(macosx):
   import windy/platforms/macos/platform
