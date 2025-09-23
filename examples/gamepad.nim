a711f21b43d917f8a173894769e87697c5553821
Jeremie Pelletier <jeremiep@gmail.com>
2025-09-23 11:47:29 -0400

    Add example

---
+++ examples/gamepad.nim
@@ -0,0 +1,35 @@
+import opengl, windy
+
+let window = newWindow("Windy Basic", ivec2(1280, 800))
+var color = vec4(0, 0, 0, 1)
+
+window.makeContextCurrent()
+loadExtensions()
+
+proc gamepad() =
+  onGamepadConnected = proc(gamepadId: int) =
+    echo "Gamepad ", gamepadId, " connected: ", gamepadName(gamepadId)
+  onGamepadDisconnected = proc(gamepadId: int) =
+    echo "Gamepad ", gamepadId, " disconnected"
+
+  for i in 0..<maxGamepads:
+    for btn in 0.GamepadButton..<GamepadButtonCount:
+      if gamepadButtonPressed(i, btn):
+        echo "Gamepad ", i, " button ", btn, " pressed"
+      if gamepadButtonReleased(i, btn):
+        echo "Gamepad ", i, " button ", btn, " released"
+      if gamepadButtonPressure(i, btn) > 0 and gamepadButtonPressure(i, btn) < 1:
+        echo "Gamepad ", i, " button ", btn, " pressure ", gamepadButtonPressure(i, btn)
+    for axis in 0.GamepadAxis..<GamepadAxisCount:
+      if gamepadAxis(i, axis) != 0:
+        echo "Gamepad ", i, " axis ", axis, " value ", gamepadAxis(i, axis)
+
+proc display() =
+  glClearColor(color.x, color.y, color.z, color.w)
+  glClear(GL_COLOR_BUFFER_BIT)
+  window.swapBuffers()
+
+while not window.closeRequested:
+  gamepad()
+  display()
+  pollEvents()
